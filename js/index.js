07798b231710282b5cff4d3024fffad97ea18911
Joycemuniu <muniurashi345@gmail.com>
2022-04-01 08:54:01 +0300

    new

--- js/index.js
+++ js/index.js
@@ -1,18 +1,68 @@
-// Nav hamburgerburger selections
-
-const burger = document.querySelector("#burger-menu");
-const ul = document.querySelector("nav ul");
-const nav = document.querySelector("nav");
-burger.addEventListener("click", () => {
-    ul.classList.toggle("show");
+$(document).ready(function(){
+  $(window).scroll(function(){
+      // sticky navbar on scroll script
+      if(this.scrollY > 20){
+          $('.navbar').addClass("sticky");
+      }else{
+          $('.navbar').removeClass("sticky");
+      }
+      
+      // scroll-up button show/hide script
+      if(this.scrollY > 500){
+          $('.scroll-up-btn').addClass("show");
+      }else{
+          $('.scroll-up-btn').removeClass("show");
+      }
   });
-  // Close hamburger menu when a link is clicked
+  // slide-up script
+  $('.scroll-up-btn').click(function(){
+      $('html').animate({scrollTop: 0});
+      // removing smooth scroll on slide-up button click
+      $('html').css("scrollBehavior", "auto");
+  });
+  $('.navbar .menu li a').click(function(){
+      // applying again smooth scroll on menu items click
+      $('html').css("scrollBehavior", "smooth");
+  });
+  // toggle menu/navbar script
+  $('.menu-btn').click(function(){
+      $('.navbar .menu').toggleClass("active");
+      $('.menu-btn i').toggleClass("active");
+  });
+  // typing text animation script
+  var typed = new Typed(".typing", {
+      strings: ["JOYCE MUNIU","programming student"],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true
+  });
+  var typed = new Typed(".typing-2", {
+      strings: ["JOYCE MUNIU","programming student"],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true
+  });
+  // owl carousel script
+  $('.carousel').owlCarousel({
+      margin: 20,
+      loop: true,
+      autoplay: true,
+      autoplayTimeOut: 2000,
+      autoplayHoverPause: true,
+      responsive: {
+          0:{
+              items: 1,
+              nav: false
+          },
+          600:{
+              items: 2,
+              nav: false
+          },
+          1000:{
+              items: 3,
+              nav: false
+          }
+      }
+  });
+});
 
-// Select nav links
-const navLink = document.querySelectorAll(".nav-link");
-
-navLink.forEach((link) =>
-  link.addEventListener("click", () => {
-    ul.classList.remove("show");
-  })
-);
